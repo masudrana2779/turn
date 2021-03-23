99dc9f35d33588fbd40db1ba0471229b220439f4
Masud Rana <masudrana2779@gmail.com>
2021-03-23 15:49:23 +0600

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,136 +4,169 @@
 
     $(document).ready(function () {
 
+        if ($('.lightbox').length > 0) {
 
-        /*===========Portfolio isotope js===========*/
-        function filTeTiSotOp() {
-            var $grid = $('.serviceImgWrap');
-            if ($grid.length) {
-                $grid.imagesLoaded(function () {
-                    $grid.isotope({
-                        itemSelector: '.serviseSingleItem',
-                        percentPosition: true,
-                        layoutMode: 'masonry',
-                        filter: "*",
-                        animationOptions: {
-                            duration: 1000
-                        },
-                        stagger: 0,
-                        transitionDuration: '0.9s',
-                        masonry: {
-                            // use outer width of grid-sizer for columnWidth
-                            columnWidth: 1
-                        }
+            /*---------LIGHT-BOX js-----------*/
+
+            function lightBoxImages() {
+
+                var selectorG = '.lightbox';
+                if ($(selectorG).length) {
+                    var instanceG = $(selectorG).imageLightbox({
+                        quitOnDocClick: false,
+                        button: true,
+                        activity: true,
+                        overlay: true,
+                        arrows: true,
+                        preloadNext: true,
                     });
-                    $(".filter_menu li").on('click', function () {
-                        $(".filter_menu li").removeClass("active");
-                        $(this).addClass("active");
+                }
+            }
 
-                        var selector = $(this).attr("data-filter");
+            lightBoxImages();
+        }
+
+
+        if ($('.serviceImgWrap').length > 0) {
+
+            /*===========Portfolio isotope js===========*/
+            function filTeTiSotOp() {
+                var $grid = $('.serviceImgWrap');
+                if ($grid.length) {
+                    $grid.imagesLoaded(function () {
                         $grid.isotope({
-                            filter: selector,
+                            itemSelector: '.serviseSingleItem',
+                            percentPosition: true,
+                            layoutMode: 'masonry',
+                            filter: "*",
                             animationOptions: {
-                                animationDuration: 750,
-                                easing: 'linear',
-                                queue: false,
+                                duration: 1000
+                            },
+                            stagger: 0,
+                            transitionDuration: '0.9s',
+                            masonry: {
+                                // use outer width of grid-sizer for columnWidth
+                                columnWidth: 1
                             }
                         });
-                        return false;
-                    });
-                });
-            }
-        }
+                        $(".filter_menu li").on('click', function () {
+                            $(".filter_menu li").removeClass("active");
+                            $(this).addClass("active");
 
-        filTeTiSotOp();
+                            var selector = $(this).attr("data-filter");
+                            $grid.isotope({
+                                filter: selector,
+                                animationOptions: {
+                                    animationDuration: 750,
+                                    easing: 'linear',
+                                    queue: false,
+                                }
+                            });
+                            return false;
+                        });
+                    });
+                }
+            }
+
+            filTeTiSotOp();
+
+        }
 
     });
+
+
     $(document).ready(function () {
 
-        function roundTextContent(selector) {
 
-            let degreeToRadian = (angle) => {
-                return angle * (Math.PI / 180);
-            };
+        if ($('.circular-text1').length > 0) {
 
-            let radius = 40;
-            let diameter = radius * 2;
-            let circle = document.querySelector(selector);
-            circle.style.width = `${diameter}px`;
-            circle.style.height = `${diameter}px`;
+            function roundTextContent(selector) {
 
-            let text = circle.innerText;
-            let characters = text.split("");
-            circle.innerText = null;
+                let degreeToRadian = (angle) => {
+                    return angle * (Math.PI / 180);
+                };
 
-            let startAngle = -90;
-            let endAngle = 270;
-            let angleRange = endAngle - startAngle;
+                let radius = 40;
+                let diameter = radius * 2;
+                let circle = document.querySelector(selector);
+                circle.style.width = `${diameter}px`;
+                circle.style.height = `${diameter}px`;
 
-            let deltaAngle = angleRange / characters.length;
-            let currentAngle = startAngle;
+                let text = circle.innerText;
+                let characters = text.split("");
+                circle.innerText = null;
 
-            characters.forEach((char, index) => {
-                let charElement = document.createElement("span");
-                charElement.innerText = char;
-                let xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
-                let yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+                let startAngle = -90;
+                let endAngle = 270;
+                let angleRange = endAngle - startAngle;
 
-                let transform = `translate(${xPos}px, ${yPos}px)`;
-                let rotate = `rotate(${index * deltaAngle}deg)`;
-                charElement.style.transform = `${transform} ${rotate}`;
+                let deltaAngle = angleRange / characters.length;
+                let currentAngle = startAngle;
 
-                currentAngle += deltaAngle;
-                circle.appendChild(charElement);
-            });
+                characters.forEach((char, index) => {
+                    let charElement = document.createElement("span");
+                    charElement.innerText = char;
+                    let xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+                    let yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+                    let transform = `translate(${xPos}px, ${yPos}px)`;
+                    let rotate = `rotate(${index * deltaAngle}deg)`;
+                    charElement.style.transform = `${transform} ${rotate}`;
+
+                    currentAngle += deltaAngle;
+                    circle.appendChild(charElement);
+                });
+            }
+
+            roundTextContent('.circular-text1');
+
         }
 
-        roundTextContent('.circular-text1');
-        $('.resetTilt').tilt({
-            reset: false
-        })
-        $('.glareTilt').tilt({
-            glare: true,
-            maxGlare: .5
-        })
+        if ($('.resetTilt').length > 0) {
+            $('.resetTilt').tilt({
+                reset: false
+            })
+            $('.glareTilt').tilt({
+                glare: true,
+                maxGlare: .5
+            })
 
-        $('.ZoomTilt').tilt({
-            scale: 1.2
-        })
-
-        function allPagepiling() {
-            $('#pagepiling').pagepiling({
-                menu: '#menu',
-                anchors: ['page1', 'page2', 'page3', 'page4'],
-                navigation: {
-                    'textColor': '#f2f2f2',
-                    'bulletsColor': '#ccc',
-                    'position': 'right',
-                }
-            });
+            $('.ZoomTilt').tilt({
+                scale: 1.2
+            })
         }
 
-        allPagepiling();
+        if ($('#pagepiling').length > 0) {
+            function allPagepiling() {
+                $('#pagepiling').pagepiling({
+                    menu: '#menu',
+                    anchors: ['page1', 'page2', 'page3', 'page4'],
+                    navigation: {
+                        'textColor': '#f2f2f2',
+                        'bulletsColor': '#ccc',
+                        'position': 'right',
+                    }
+                });
+            }
 
+            allPagepiling();
+            $('.allBody').hide();
+            $('.banner_1 .scrollDownAnimationOuter').hide();
+            $('body').css({
+                overflow: 'hidden',
+                paddingRight: '17px'
+            }).addClass("pp-viewing-page1");
 
+            function addClassbanner_1() {
+                $('.allBody').fadeIn(1500);
+                $('.banner_1 .scrollDownAnimationOuter').fadeIn(2000);
+                $('body').removeAttr("style");
+            }
 
-        $('.allBody').hide();
-        $('.banner_1 .scrollDownAnimationOuter').hide();
-        $('body').css({
-            overflow: 'hidden',
-            paddingRight: '17px'
-        }).addClass("pp-viewing-page1");
-
-        function addClassbanner_1() {
-            $('.allBody').fadeIn(1500);
-            $('.banner_1 .scrollDownAnimationOuter').fadeIn(2000);
-            $('body').removeAttr("style");
+            setTimeout(function () {
+                addClassbanner_1();
+            }, 4000);
         }
-
-        setTimeout(function () {
-            addClassbanner_1();
-        }, 4000);
-
-
 
 
     });
