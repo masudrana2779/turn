20cf1ab939f98a75ffc68c08880fc89bc680b293
Masud Rana <masudrana2779@gmail.com>
2021-03-24 18:37:57 +0600

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,49 @@
 
 
     $(document).ready(function () {
+        if ($('.circular-text1').length > 0) {
+            function roundTextContent(selector) {
 
+                let degreeToRadian = (angle) => {
+                    return angle * (Math.PI / 180);
+                };
 
+                let radius = 40;
+                let diameter = radius * 2;
+                let circle = document.querySelector(selector);
+                circle.style.width = `${diameter}px`;
+                circle.style.height = `${diameter}px`;
+
+                let text = circle.innerText;
+                let characters = text.split("");
+                circle.innerText = null;
+
+                let startAngle = -90;
+                let endAngle = 270;
+                let angleRange = endAngle - startAngle;
+
+                let deltaAngle = angleRange / characters.length;
+                let currentAngle = startAngle;
+
+                characters.forEach((char, index) => {
+                    let charElement = document.createElement("span");
+                    charElement.innerText = char;
+                    let xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+                    let yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+                    let transform = `translate(${xPos}px, ${yPos}px)`;
+                    let rotate = `rotate(${index * deltaAngle}deg)`;
+                    charElement.style.transform = `${transform} ${rotate}`;
+
+                    currentAngle += deltaAngle;
+                    circle.appendChild(charElement);
+                });
+            }
+            roundTextContent('.circular-text1');
+        }
+    });
+
+    $(document).ready(function () {
 
         // headerFixed
         function headerFixed() {
@@ -19,7 +60,7 @@
         headerFixed();
         $(window).scroll(function (){
             headerFixed();
-        })
+        });
 
 
         if ($('.lightbox').length > 0) {
@@ -89,46 +130,6 @@
 
         }
 
-        if ($('.circular-text1').length > 0) {
-            function roundTextContent(selector) {
-
-                let degreeToRadian = (angle) => {
-                    return angle * (Math.PI / 180);
-                };
-
-                let radius = 40;
-                let diameter = radius * 2;
-                let circle = document.querySelector(selector);
-                circle.style.width = `${diameter}px`;
-                circle.style.height = `${diameter}px`;
-
-                let text = circle.innerText;
-                let characters = text.split("");
-                circle.innerText = null;
-
-                let startAngle = -90;
-                let endAngle = 270;
-                let angleRange = endAngle - startAngle;
-
-                let deltaAngle = angleRange / characters.length;
-                let currentAngle = startAngle;
-
-                characters.forEach((char, index) => {
-                    let charElement = document.createElement("span");
-                    charElement.innerText = char;
-                    let xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
-                    let yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
-
-                    let transform = `translate(${xPos}px, ${yPos}px)`;
-                    let rotate = `rotate(${index * deltaAngle}deg)`;
-                    charElement.style.transform = `${transform} ${rotate}`;
-
-                    currentAngle += deltaAngle;
-                    circle.appendChild(charElement);
-                });
-            }
-            roundTextContent('.circular-text1');
-        }
 
 
         var windowWidth = $(window).width();
